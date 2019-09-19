e7cc4c14679674bd19b97deeefdad448bbeb5a31
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-09-19 15:49:08 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>Home</h1>
+    <p>Hello Frontend Masters!</p>
+    <Link to="/about">About Page</Link>
+  </>
+);
