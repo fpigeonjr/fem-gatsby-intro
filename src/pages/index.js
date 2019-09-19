6e48b9c9f9a500604c7ccb21fbc6c6be9fbf8c9c
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-09-19 16:17:55 -0500

    starts layout for pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Hello Frontend Masters!</p>
     <Link to="/about">About Page</Link>
-  </>
+  </Layout>
 );
