1327dfa67c29d280c7f6c6d0beab177c0f63f889
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-09-20 15:47:15 -0500

    adds  header to the layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Header from './header';
 
 const Layout = ({ children }) => (
   <>
@@ -47,7 +48,7 @@ const Layout = ({ children }) => (
         }
       `}
     />
-    <header></header>
+    <Header />
     <main
       css={css`
         margin: 2rem auto 4rem;
