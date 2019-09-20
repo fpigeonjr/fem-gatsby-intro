320eebb403bdf555887e655b02be60991ac5f023
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-09-20 16:28:43 -0500

    adds site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'FEM Gatsby Workshop',
+    description: 'site we built together at FEM',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,64 +1,75 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
 import Header from './header';
+import Helmet from 'react-helmet';
+import useSiteMetaData from '../components/hooks/use-sitemetadata';
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
-
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-          font-size: 18px;
-          line-height: 1.4;
-          /* remove margin for the main div */
-          > div {
-            margin-top: 0;
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetaData();
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
           }
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            color: #222;
-            line-height: 1.1;
-            + * {
-              margin-top: 0.5rem;
+
+          * + * {
+            margin-top: 1rem;
+          }
+
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
+            /* remove margin for the main div */
+            > div {
+              margin-top: 0;
+            }
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+              color: #222;
+              line-height: 1.1;
+              + * {
+                margin-top: 0.5rem;
+              }
+            }
+            strong {
+              color: #222;
+            }
+            li {
+              margin-top: 0.25rem;
             }
           }
-          strong {
-            color: #222;
-          }
-          li {
-            margin-top: 0.25rem;
-          }
-        }
-      `}
-    />
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </>
-);
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
 
 export default Layout;
