11dd1b68581c0e3fad600b84ac41ddd6f5889e08
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-09-21 07:45:10 -0500

    adds a hero image to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,20 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import PostPreview from '../components/post-preview';
 import usePosts from '../components/hooks/use-posts';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Frontend Masters!</p>
-      <Link to="/about">About Page</Link>
-      <h2>Read My Blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <Link to="/about">About Page</Link>
+        <h2>Read My Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
