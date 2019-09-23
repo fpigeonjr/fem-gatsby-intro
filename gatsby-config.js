7f553e796e8f0c1267e360ab641e36e03eaa7f98
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-09-23 16:09:31 -0500

    adds instagram images to the homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,11 @@ module.exports = {
         path: 'images',
       },
     },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'gatsbyjs',
+      },
+    },
   ],
 };
---
+++ src/components/insta.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { css } from '@emotion/core';
+import useInstagram from '../components/hooks/use-instagram';
+
+const Insta = () => {
+  const instaPhotos = useInstagram();
+  const { username } = instaPhotos[0];
+
+  return (
+    <>
+      <h2>Instagram post from @{username}</h2>
+      <div
+        css={css`
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          margin: 1rem -0.5rem;
+        `}
+      >
+        {instaPhotos.map(photo => (
+          <a
+            href={`https://instagram.com/p/${photo.id}`}
+            css={css`
+              box-shadow: 0;
+              display: block;
+              margin: 0.5rem;
+              max-width: calc(33% - 1rem);
+              width: 120px;
+              transition: 200ms box-shadow linear;
+              :focus,
+              :hover {
+                box-shadow: 0 2px 14px #22222244;
+                z-index: 10;
+              }
+            `}
+          >
+            <Image
+              key={photo.id}
+              fluid={photo.fluid}
+              alt={photo.caption}
+              css={css`
+                width: 100%;
+                * {
+                  margin-top: 0;
+                }
+              `}
+            ></Image>
+          </a>
+        ))}
+        <a href={`https://instagram.com/${username}`}>
+          See More on Instagram &rarr;
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Insta;
